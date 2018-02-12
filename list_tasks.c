96831a65f7387a306c9dc4a6f97f7a94d27b067c
Ram <ramhari.dahal1@gmail.com>
2018-02-12 06:19:15 -0700

    Info for parents

--- list_tasks.c
+++ list_tasks.c
@@ -9,8 +9,15 @@ int list_tasks_init(void) {
 	printk(KERN_INFO "Loading module\n");
 	for_each_process(task) {
 		printk(KERN_INFO "pid: %d, pname: %s, state: %ld\n", task->pid, task->comm, task->state); 
-	}
 	
+
+		if (task->parent) {
+			struct task_struct *t_parent;
+			t_parent = task->parent;
+			printk(KERN_INFO "parent pid: %d, parent pname: %s, parent state: %ld\n", t_parent->pid, t_parent->comm, t_parent->state);
+		}
+	
+	}
 	return 0;	
 }
 
