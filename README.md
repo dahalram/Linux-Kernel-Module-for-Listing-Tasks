4a29d0887e554c8260fd48519298f7e4d482f935
Ram <ramhari.dahal1@gmail.com>
2018-02-13 20:55:06 -0700

    For grandparents and so on

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Linux-Kernel-Module-for-Listing-Tasks
+The file fork_process.c is for problem 3.17. 
+
+The file list_tasks.c has the code for listing tasks. The order of how the tasks are listed as follows:
+Child : pname, state, pid, 
+Parent 0: Parent of the child above,
+Parent 1: Grandparent of Child, Parent of Parent 0,
+Parent [i]: Parent of Parent [i-1] 
+and so on. 
--- list_tasks.c
+++ list_tasks.c
@@ -4,20 +4,31 @@
 #include <linux/module.h>
 
 int list_tasks_init(void) {
+	
 	struct task_struct *task;
 		
 	printk(KERN_INFO "Loading module\n");
 	for_each_process(task) {
-		printk(KERN_INFO "pid: %d, pname: %s, state: %ld\n", task->pid, task->comm, task->state); 
+		printk(KERN_INFO "Child:: pname: %s, state: %ld, pid: %d\n", task->comm, task->state, task->pid); 
 	
-
+		unsigned int i = 0;
 		if (task->parent) {
 			struct task_struct *t_parent;
 			t_parent = task->parent;
-			printk(KERN_INFO "parent pid: %d, parent pname: %s, parent state: %ld\n", t_parent->pid, t_parent->comm, t_parent->state);
+			//printk(KERN_INFO "parent pid: %d, parent pname: %s, parent state: %ld\n", t_parent->pid, t_parent->comm, t_parent->state);
+		
+		
+			while (t_parent->pid != 0) {
+				//struct task_struct *t_parent; 
+				//t_parent = task->parent;
+				printk(KERN_INFO "Parent %d :: pname: %s, parent state: %ld, pid: %d\n", i, t_parent->comm, t_parent->state, t_parent->pid);
+				i = i + 1;
+				t_parent = t_parent->parent;
+			}
 		}
 	
 	}
+
 	return 0;	
 }
 
